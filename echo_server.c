31dc7c7adc9851258a0275e12eb16706bb46f066
Masaya YAMAMOTO <pandax381@gmail.com>
2013-04-26 15:31:36 +0900

    update

--- echo_server.c
+++ echo_server.c
@@ -24,55 +24,39 @@ struct microps_param param = {
 int
 main (int argc, char *argv[]) {
 	int soc, acc;
-    ip_addr_t peer;
 	uint8_t buf[65536];
 	ssize_t len;
 
 	microps_init(&param);
 	soc = tcp_api_open();
     if (soc == -1) {
-fprintf(stderr, "err1\n");
-        microps_cleanup();
-        return -1;
+        goto ERROR;
 	}
-/*
     if (tcp_api_bind(soc, 7) == -1) {
-fprintf(stderr, "err2\n");
-        tcp_api_close(soc);
-        microps_cleanup();
-        return -1;
+        goto ERROR;
     }
     tcp_api_listen(soc);
     acc = tcp_api_accept(soc);
     if (acc == -1) {
-fprintf(stderr, "err3\n");
-        tcp_api_close(soc);
-        microps_cleanup();
-        return -1;
+        goto ERROR;
     }
 fprintf(stderr, "accept success, soc=%d, acc=%d\n", soc, acc);
-*/
-    ip_addr_pton("10.10.2.98", &peer);
-    if (tcp_api_connect(soc, &peer, 7) == -1) {
-fprintf(stderr, "err2\n");
-        tcp_api_close(soc);
-        microps_cleanup();
-        return -1;
-    }
-fprintf(stderr, "connect success\n");
 	while (1) {
-		len = tcp_api_recv(soc, buf, sizeof(buf));
-fprintf(stderr, "tcp_api_recv(): %ld\n", len);
+		len = tcp_api_recv(acc, buf, sizeof(buf));
 		if (len <= 0) {
 			break;
 		}
 		hexdump(stderr, buf, len);
-		tcp_api_send(soc, buf, len);
+		tcp_api_send(acc, buf, len);
 	}
-/*
 	tcp_api_close(acc);
-*/
 	tcp_api_close(soc);
 	microps_cleanup();
     return  0;
+ERROR:
+    if (soc != -1) {
+        tcp_api_close(soc);
+    }
+    microps_cleanup();
+    return -1;
 }
