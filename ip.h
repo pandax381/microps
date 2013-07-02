03d72e2d38d15f833c9f3bfd2280bd64d3a43f0f
Masaya YAMAMOTO <pandax381@gmail.com>
2013-07-02 15:49:18 +0900

    update

--- ip.h
+++ ip.h
@@ -21,7 +21,9 @@ typedef uint32_t ip_addr_t;
 typedef void (*__ip_protocol_handler_t)(uint8_t *, size_t, ip_addr_t *, ip_addr_t *);
 
 extern int
-ip_init (const char *addr, const char *netmask, const char *gateway);
+ip_addr_pton (const char *p, ip_addr_t *n);
+extern char *
+ip_addr_ntop (const ip_addr_t *n, char *p, size_t size);
 extern ip_addr_t
 ip_get_addr (ip_addr_t *dst);
 extern int
@@ -29,8 +31,6 @@ ip_add_protocol (uint8_t protocol, __ip_protocol_handler_t handler);
 extern ssize_t
 ip_output (uint8_t protocol, const uint8_t *buf, size_t len, const ip_addr_t *addr);
 extern int
-ip_addr_pton (const char *p, ip_addr_t *n);
-extern char *
-ip_addr_ntop (const ip_addr_t *n, char *p, size_t size);
+ip_init (const char *addr, const char *netmask, const char *gateway);
 
 #endif
