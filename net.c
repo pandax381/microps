2e717204b84dbef5e3d1711e178d84c25e4cfc11
YAMAMOTO Masaya <pandax381@gmail.com>
2021-01-07 13:48:18 +0900

    Add interface management mechanism

--- net.c
+++ net.c
@@ -99,6 +99,37 @@ net_device_close(struct net_device *dev)
     return 0;
 }
 
+/* NOTE: must not be call after net_run() */
+int
+net_device_add_iface(struct net_device *dev, struct net_iface *iface)
+{
+    struct net_iface *entry;
+
+    for (entry = dev->ifaces; entry; entry = entry->next) {
+        if (entry->family == iface->family) {
+            errorf("already exists, dev=%s, family=%d", dev->name, entry->family);
+            return -1;
+        }
+    }
+    iface->next = dev->ifaces;
+    iface->dev = dev;
+    dev->ifaces = iface;
+    return 0;
+}
+
+struct net_iface *
+net_device_get_iface(struct net_device *dev, int family)
+{
+    struct net_iface *entry;
+
+    for (entry = dev->ifaces; entry; entry = entry->next) {
+        if (entry->family == family) {
+            break;
+        }
+    }
+    return entry;
+}
+
 int
 net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst)
 {
--- net.h
+++ net.h
@@ -23,12 +23,24 @@
 #define NET_DEVICE_IS_UP(x) ((x)->flags & NET_DEVICE_FLAG_UP)
 #define NET_DEVICE_STATE(x) (NET_DEVICE_IS_UP(x) ? "up" : "down")
 
+#define NET_IFACE_FAMILY_IP    1
+#define NET_IFACE_FAMILY_IPV6  2
+
+#define NET_IFACE(x) ((struct net_iface *)(x))
+
 /* NOTE: use same value as the Ethernet types */
 #define NET_PROTOCOL_TYPE_IP   0x0800
 #define NET_PROTOCOL_TYPE_ARP  0x0806
 #define NTT_PROTOCOL_TYPE_IPV6 0x86dd
 
 struct net_device; /* forward declaration */
+
+struct net_iface {
+    struct net_iface *next;
+    struct net_device *dev;
+    int family;
+    /* depends on implementation of protocols. */
+};
 
 struct net_device_ops {
     int (*open)(struct net_device *dev);
@@ -39,6 +51,7 @@ struct net_device_ops {
 
 struct net_device {
     struct net_device *next;
+    struct net_iface *ifaces; /* NOTE: if you want to add/delete the entries after net_run(), you need to protect ifaces with a mutex. */
     unsigned int index;
     char name[IFNAMSIZ];
     uint16_t type;
@@ -59,6 +72,10 @@ extern struct net_device *
 net_device_alloc(void (*setup)(struct net_device *dev));
 extern int
 net_device_register(struct net_device *dev);
+extern int
+net_device_add_iface(struct net_device *dev, struct net_iface *iface);
+extern struct net_iface *
+net_device_get_iface(struct net_device *dev, int family);
 extern int
 net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst);
 
