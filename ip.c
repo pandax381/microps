0402a8593f0ba103ddeb74552d38414ba2629c71
YAMAMOTO Masaya <pandax381@gmail.com>
2021-02-27 01:14:58 +0900

    step5

--- ip.c
+++ ip.c
@@ -1,15 +1,73 @@
+#include <stdio.h>
 #include <stdint.h>
-#include <stddef.h>
+#include <stdlib.h>
 
 #include "util.h"
 #include "net.h"
 #include "ip.h"
 
+struct ip_hdr {
+    uint8_t vhl;
+    uint8_t tos;
+    uint16_t total;
+    uint16_t id;
+    uint16_t offset;
+    uint8_t ttl;
+    uint8_t protocol;
+    uint16_t sum;
+    ip_addr_t src;
+    ip_addr_t dst;
+    uint8_t options[0];
+};
+
+const ip_addr_t IP_ADDR_ANY       = 0x00000000; /* 0.0.0.0 */
+const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
+
+int
+ip_addr_pton(const char *p, ip_addr_t *n)
+{
+    char *sp, *ep;
+    int idx;
+    long ret;
+
+    sp = (char *)p;
+    for (idx = 0; idx < 4; idx++) {
+        ret = strtol(sp, &ep, 10);
+        if (ret < 0 || ret > 255) {
+            return -1;
+        }
+        if (ep == sp) {
+            return -1;
+        }
+        if ((idx == 3 && *ep != '\0') || (idx != 3 && *ep != '.')) {
+            return -1;
+        }
+        ((uint8_t *)n)[idx] = ret;
+        sp = ep + 1;
+    }
+    return 0;
+}
+
+char *
+ip_addr_ntop(const ip_addr_t n, char *p, size_t size)
+{
+    uint8_t *u8;
+
+    u8 = (uint8_t *)&n;
+    snprintf(p, size, "%d.%d.%d.%d", u8[0], u8[1], u8[2], u8[3]);
+    return p;
+}
+
+void
+ip_dump(const uint8_t *data, size_t len)
+{
+
+}
+
 static void
 ip_input(const uint8_t *data, size_t len, struct net_device *dev)
 {
-    debugf("dev=%s, len=%zu", dev->name, len);
-    debugdump(data, len);
+
 }
 
 int
