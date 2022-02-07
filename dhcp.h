ed6ad994281204788462aaa13f0558c34ed1e261
YAMAMOTO Masaya <pandax381@gmail.com>
2022-02-07 14:49:06 +0900

    Destruction

--- dhcp.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DHCP_H_
-#define _DHCP_H_
-
-#include "net.h"
-
-extern int
-dhcp_init (struct netif *iface);
-
-#endif
--- microps.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MICROPS_H_
-#define _MICROPS_H_
-
-extern int
-microps_init (void);
-extern void
-microps_cleanup (void);
-
-#endif
