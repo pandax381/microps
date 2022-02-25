033ccf76b2496b1ee7bee04d47cf930e6746683e
YAMAMOTO Masaya <pandax381@gmail.com>
2022-02-26 08:08:01 +0900

    completion of step13

--- net.c
+++ net.c
@@ -247,6 +247,7 @@ net_shutdown(void)
     debugf("shutting down");
 }
 
+#include "arp.h"
 #include "ip.h"
 #include "icmp.h"
 
@@ -255,6 +256,10 @@ net_init(void)
 {
     if (intr_init() == -1) {
         errorf("intr_init() failure");
+        return -1;
+    }
+    if (arp_init() == -1) {
+        errorf("arp_init() failure");
         return -1;
     }
     if (ip_init() == -1) {
