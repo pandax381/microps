1bdb1f95c5f0b736beea9b0ba7df2ec3454fbb44
YAMAMOTO Masaya <pandax381@gmail.com>
2022-02-26 06:06:42 +0900

    completion of step1

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
 APPS = 
 
-DRIVERS = 
+DRIVERS = driver/dummy.o \
 
 OBJS = util.o \
+       net.o \
 
 TESTS = test/step0.exe \
+        test/step1.exe \
 
 CFLAGS := $(CFLAGS) -g -W -Wall -Wno-unused-parameter -iquote .
 
--- driver/dummy.c
+++ driver/dummy.c
@@ -10,6 +10,10 @@
 static int
 dummy_transmit(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst)
 {
+    debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
+    debugdump(data, len);
+    /* drop data */
+    return 0;
 }
 
 static struct net_device_ops dummy_ops = {
@@ -19,4 +23,22 @@ static struct net_device_ops dummy_ops = {
 struct net_device *
 dummy_init(void)
 {
+    struct net_device *dev;
+
+    dev = net_device_alloc();
+    if (!dev) {
+        errorf("net_device_alloc() failure");
+        return NULL;
+    }
+    dev->type = NET_DEVICE_TYPE_DUMMY;
+    dev->mtu = DUMMY_MTU;
+    dev->hlen = 0; /* non header */
+    dev->alen = 0; /* non address */
+    dev->ops = &dummy_ops;
+    if (net_device_register(dev) == -1) {
+        errorf("net_device_register() failure");
+        return NULL;
+    }
+    debugf("initialized, dev=%s", dev->name);
+    return dev;
 }
