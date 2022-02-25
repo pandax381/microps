a36c6776bfaacda4a57528d2b7496a38fbcae5e5
YAMAMOTO Masaya <pandax381@gmail.com>
2022-02-26 08:09:58 +0900

    step14

--- arp.c
+++ arp.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <sys/time.h>
+
+#include "platform.h"
 
 #include "util.h"
 #include "net.h"
@@ -15,6 +18,13 @@
 
 #define ARP_OP_REQUEST 1
 #define ARP_OP_REPLY   2
+
+#define ARP_CACHE_SIZE 32
+
+#define ARP_CACHE_STATE_FREE       0
+#define ARP_CACHE_STATE_INCOMPLETE 1
+#define ARP_CACHE_STATE_RESOLVED   2
+#define ARP_CACHE_STATE_STATIC     3
 
 struct arp_hdr {
     uint16_t hrd;
@@ -31,6 +41,16 @@ struct arp_ether_ip {
     uint8_t tha[ETHER_ADDR_LEN];
     uint8_t tpa[IP_ADDR_LEN];
 };
+
+struct arp_cache {
+    unsigned char state;
+    ip_addr_t pa;
+    uint8_t ha[ETHER_ADDR_LEN];
+    struct timeval timestamp;
+};
+
+static mutex_t mutex = MUTEX_INITIALIZER;
+static struct arp_cache caches[ARP_CACHE_SIZE];
 
 static char *
 arp_opcode_ntoa(uint16_t opcode)
@@ -68,6 +88,37 @@ arp_dump(const uint8_t *data, size_t len)
     hexdump(stderr, data, len);
 #endif
     funlockfile(stderr);
+}
+
+/*
+ * ARP Cache
+ *
+ * NOTE: ARP Cache functions must be called after mutex locked
+ */
+
+static void
+arp_cache_delete(struct arp_cache *cache)
+{
+}
+
+static struct arp_cache *
+arp_cache_alloc(void)
+{
+}
+
+static struct arp_cache *
+arp_cache_select(ip_addr_t pa)
+{
+}
+
+static struct arp_cache *
+arp_cache_update(ip_addr_t pa, const uint8_t *ha)
+{
+}
+
+static struct arp_cache *
+arp_cache_insert(ip_addr_t pa, const uint8_t *ha)
+{
 }
 
 static int
@@ -119,6 +170,11 @@ arp_input(const uint8_t *data, size_t len, struct net_device *dev)
             arp_reply(iface, msg->sha, spa, msg->sha);
         }
     }
+}
+
+int
+arp_resolve(struct net_iface *iface, ip_addr_t pa, uint8_t *ha)
+{
 }
 
 int
