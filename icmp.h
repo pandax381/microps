4655617ecc18815543fa8c4dc20cd76e74cf9fd1
YAMAMOTO Masaya <pandax381@gmail.com>
2022-02-26 07:40:16 +0900

    step11

--- icmp.h
+++ icmp.h
@@ -1,6 +1,11 @@
 #ifndef ICMP_H
 #define ICMP_H
 
+#include <stddef.h>
+#include <stdint.h>
+
+#include "ip.h"
+
 #define ICMP_HDR_SIZE 8
 
 #define ICMP_TYPE_ECHOREPLY           0
@@ -14,6 +19,9 @@
 #define ICMP_TYPE_TIMESTAMPREPLY     14
 #define ICMP_TYPE_INFO_REQUEST       15
 #define ICMP_TYPE_INFO_REPLY         16
+
+extern int
+icmp_output(uint8_t type, uint8_t code, uint32_t values, const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst);
 
 extern int
 icmp_init(void);
