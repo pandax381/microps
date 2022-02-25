48c1695deb9d764b9fd712bf09d50dae8014ebc3
YAMAMOTO Masaya <pandax381@gmail.com>
2022-02-26 07:28:51 +0900

    step10

--- icmp.c
+++ icmp.c
@@ -5,6 +5,55 @@
 #include "ip.h"
 #include "icmp.h"
 
+struct icmp_hdr {
+    uint8_t type;
+    uint8_t code;
+    uint16_t sum;
+    uint32_t values;
+};
+
+struct icmp_echo {
+    uint8_t type;
+    uint8_t code;
+    uint16_t sum;
+    uint16_t id;
+    uint16_t seq;
+};
+
+static char *
+icmp_type_ntoa(uint8_t type) {
+    switch (type) {
+    case ICMP_TYPE_ECHOREPLY:
+        return "EchoReply";
+    case ICMP_TYPE_DEST_UNREACH:
+        return "DestinationUnreachable";
+    case ICMP_TYPE_SOURCE_QUENCH:
+        return "SourceQuench";
+    case ICMP_TYPE_REDIRECT:
+        return "Redirect";
+    case ICMP_TYPE_ECHO:
+        return "Echo";
+    case ICMP_TYPE_TIME_EXCEEDED:
+        return "TimeExceeded";
+    case ICMP_TYPE_PARAM_PROBLEM:
+        return "ParameterProblem";
+    case ICMP_TYPE_TIMESTAMP:
+        return "Timestamp";
+    case ICMP_TYPE_TIMESTAMPREPLY:
+        return "TimestampReply";
+    case ICMP_TYPE_INFO_REQUEST:
+        return "InformationRequest";
+    case ICMP_TYPE_INFO_REPLY:
+        return "InformationReply";
+    }
+    return "Unknown";
+}
+
+static void
+icmp_dump(const uint8_t *data, size_t len)
+{
+}
+
 void
 icmp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct ip_iface *iface)
 {
