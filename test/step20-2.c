0e5fe189ac959736bbbcdc6e33baebcd2019057a
YAMAMOTO Masaya <pandax381@gmail.com>
2022-03-08 01:29:20 +0900

    completation of step20

---
+++ test/step20-2.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "util.h"
+#include "net.h"
+#include "ip.h"
+#include "icmp.h"
+#include "udp.h"
+
+#include "driver/loopback.h"
+#include "driver/ether_tap.h"
+
+#include "test.h"
+
+static volatile sig_atomic_t terminate;
+
+static void
+on_signal(int s)
+{
+    (void)s;
+    terminate = 1;
+    close(0); /* close STDIN */
+}
+
+static int
+setup(void)
+{
+    struct net_device *dev;
+    struct ip_iface *iface;
+
+    signal(SIGINT, on_signal);
+    if (net_init() == -1) {
+        errorf("net_init() failure");
+        return -1;
+    }
+    dev = loopback_init();
+    if (!dev) {
+        errorf("loopback_init() failure");
+        return -1;
+    }
+    iface = ip_iface_alloc(LOOPBACK_IP_ADDR, LOOPBACK_NETMASK);
+    if (!iface) {
+        errorf("ip_iface_alloc() failure");
+        return -1;
+    }
+    if (ip_iface_register(dev, iface) == -1) {
+        errorf("ip_iface_register() failure");
+        return -1;
+    }
+    dev = ether_tap_init(ETHER_TAP_NAME, ETHER_TAP_HW_ADDR);
+    if (!dev) {
+        errorf("ether_tap_init() failure");
+        return -1;
+    }
+    iface = ip_iface_alloc(ETHER_TAP_IP_ADDR, ETHER_TAP_NETMASK);
+    if (!iface) {
+        errorf("ip_iface_alloc() failure");
+        return -1;
+    }
+    if (ip_iface_register(dev, iface) == -1) {
+        errorf("ip_iface_register() failure");
+        return -1;
+    }
+    if (ip_route_set_default_gateway(iface, DEFAULT_GATEWAY) == -1) {
+        errorf("ip_route_set_default_gateway() failure");
+        return -1;
+    }
+    if (net_run() == -1) {
+        errorf("net_run() failure");
+        return -1;
+    }
+    return 0;
+}
+
+static void
+cleanup(void)
+{
+    net_shutdown();
+}
+
+int
+main(int argc, char *argv[])
+{
+    int soc;
+    struct ip_endpoint foreign;
+    uint8_t buf[1024];
+
+    if (setup() == -1) {
+        errorf("setup() failure");
+        return -1;
+    }
+    soc = udp_open();
+    if (soc == -1) {
+        errorf("udp_open() failure");
+        return -1;
+    }
+    ip_endpoint_pton("192.0.2.1:10007", &foreign);
+    while (!terminate) {
+        if (!fgets((char *)buf, sizeof(buf), stdin)) {
+            break;
+        }
+        if (udp_sendto(soc, buf, strlen((char *)buf), &foreign) == -1) {
+            errorf("sock_sendto() failure");
+            break;
+        }
+    }
+    udp_close(soc);
+    cleanup();
+    return 0;
+}
--- udp.c
+++ udp.c
@@ -40,6 +40,7 @@ struct udp_pcb {
     int state;
     struct ip_endpoint local;
     struct queue_head queue; /* receive queue */
+    int wc; /* wait count */
 };
 
 struct udp_queue_entry {
@@ -93,6 +94,10 @@ udp_pcb_release(struct udp_pcb *pcb)
 {
     struct queue_entry *entry;
 
+    if (pcb->wc) {
+        pcb->state = UDP_PCB_STATE_CLOSING;
+        return;
+    }
     pcb->state = UDP_PCB_STATE_FREE;
     pcb->local.addr = IP_ADDR_ANY;
     pcb->local.port = 0;
@@ -320,9 +325,87 @@ udp_bind(int id, struct ip_endpoint *local)
 ssize_t
 udp_sendto(int id, uint8_t *data, size_t len, struct ip_endpoint *foreign)
 {
+    struct udp_pcb *pcb;
+    struct ip_endpoint local;
+    struct ip_iface *iface;
+    char addr[IP_ADDR_STR_LEN];
+    uint32_t p;
+
+    mutex_lock(&mutex);
+    pcb = udp_pcb_get(id);
+    if (!pcb) {
+        errorf("pcb not found, id=%d", id);
+        mutex_unlock(&mutex);
+        return -1;
+    }
+    local.addr = pcb->local.addr;
+    if (local.addr == IP_ADDR_ANY) {
+        iface = ip_route_get_iface(foreign->addr);
+        if (!iface) {
+            errorf("iface not found that can reach foreign address, addr=%s",
+                ip_addr_ntop(foreign->addr, addr, sizeof(addr)));
+            mutex_unlock(&mutex);
+            return -1;
+        }
+        local.addr = iface->unicast;
+        debugf("select local address, addr=%s", ip_addr_ntop(local.addr, addr, sizeof(addr)));
+    }
+    if (!pcb->local.port) {
+        for (p = UDP_SOURCE_PORT_MIN; p <= UDP_SOURCE_PORT_MAX; p++) {
+            if (!udp_pcb_select(local.addr, hton16(p))) {
+                pcb->local.port = hton16(p);
+                debugf("dinamic assign local port, port=%d", p);
+                break;
+            }
+        }
+        if (!pcb->local.port) {
+            debugf("failed to dinamic assign local port, addr=%s", ip_addr_ntop(local.addr, addr, sizeof(addr)));
+            mutex_unlock(&mutex);
+            return -1;
+        }
+    }
+    local.port = pcb->local.port;
+    mutex_unlock(&mutex);
+    return udp_output(&local, foreign, data, len);
 }
 
 ssize_t
 udp_recvfrom(int id, uint8_t *buf, size_t size, struct ip_endpoint *foreign)
 {
+    struct udp_pcb *pcb;
+    struct udp_queue_entry *entry;
+    ssize_t len;
+
+    mutex_lock(&mutex);
+    pcb = udp_pcb_get(id);
+    if (!pcb) {
+        errorf("pcb not found, id=%d", id);
+        mutex_unlock(&mutex);
+        return -1;
+    }
+    while (1) {
+        entry = queue_pop(&pcb->queue);
+        if (entry) {
+            break;
+        }
+        pcb->wc++;
+        mutex_unlock(&mutex);
+        sleep(1);
+        mutex_lock(&mutex);
+        pcb->wc--;
+        if (pcb->state == UDP_PCB_STATE_CLOSING) {
+            debugf("closed");
+            udp_pcb_release(pcb);
+            mutex_unlock(&mutex);
+            return -1;
+        }
+    }
+    mutex_unlock(&mutex);
+    if (foreign) {
+        *foreign = entry->foreign;
+    }
+    len = MIN(size, entry->len); /* truncate */
+    memcpy(buf, entry->data, len);
+    memory_free(entry);
+    return len;
 }
