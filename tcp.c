e0afa5adb7c6501c6f63bc48719a6997011f1111
YAMAMOTO Masaya <pandax381@gmail.com>
2022-03-09 10:32:53 +0900

    step22

---
+++ tcp.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "util.h"
+#include "ip.h"
+#include "tcp.h"
+
+#define TCP_FLG_FIN 0x01
+#define TCP_FLG_SYN 0x02
+#define TCP_FLG_RST 0x04
+#define TCP_FLG_PSH 0x08
+#define TCP_FLG_ACK 0x10
+#define TCP_FLG_URG 0x20
+
+#define TCP_FLG_IS(x, y) ((x & 0x3f) == (y))
+#define TCP_FLG_ISSET(x, y) ((x & 0x3f) & (y) ? 1 : 0)
+
+struct pseudo_hdr {
+    uint32_t src;
+    uint32_t dst;
+    uint8_t zero;
+    uint8_t protocol;
+    uint16_t len;
+};
+
+struct tcp_hdr {
+    uint16_t src;
+    uint16_t dst;
+    uint32_t seq;
+    uint32_t ack;
+    uint8_t off;
+    uint8_t flg;
+    uint16_t wnd;
+    uint16_t sum;
+    uint16_t up;
+};
+
+static char *
+tcp_flg_ntoa(uint8_t flg)
+{
+    static char str[9];
+
+    snprintf(str, sizeof(str), "--%c%c%c%c%c%c",
+        TCP_FLG_ISSET(flg, TCP_FLG_URG) ? 'U' : '-',
+        TCP_FLG_ISSET(flg, TCP_FLG_ACK) ? 'A' : '-',
+        TCP_FLG_ISSET(flg, TCP_FLG_PSH) ? 'P' : '-',
+        TCP_FLG_ISSET(flg, TCP_FLG_RST) ? 'R' : '-',
+        TCP_FLG_ISSET(flg, TCP_FLG_SYN) ? 'S' : '-',
+        TCP_FLG_ISSET(flg, TCP_FLG_FIN) ? 'F' : '-');
+    return str;
+}
+
+static void
+tcp_dump(const uint8_t *data, size_t len)
+{
+}
+
+static void
+tcp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct ip_iface *iface)
+{
+}
+
+int
+tcp_init(void)
+{
+}
